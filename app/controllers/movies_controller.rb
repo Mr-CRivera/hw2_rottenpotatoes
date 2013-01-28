13ed35ae67794428eeb8ba2559432d742e034aeb
Mr-CRivera <computerswengineer@gmail.com>
2013-01-28 14:15:46 -0800

    hw2_2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,38 @@
 class MoviesController < ApplicationController
 
+  @first = true
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
+
+
+
   def index
     if params[:format]!= nil 
       self.instance_variable_set ("@hilite_"+params[:format]).to_sym, "hilite"
     end
-    @movies = Movie.all :order => (params[:format])
+    
+    @all_ratings = Movie.ratings
+    if @first then 
+       @rat_selecc = {}
+       @all_ratings.each do |rat|
+         @rat_selecc[rat] = 1
+       end
+       @first = false
+    else
+       @rat_selecc = params[:ratings]
+    end
+    @movies = Movie.rated_and_ordered params[:ratings], params[:format]
+
+    flash[:notice] = @rat_selecc
   end
+
+
+
 
   def new
     # default: render 'new' template
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,21 @@
 class Movie < ActiveRecord::Base
+
+   def Movie.ratings
+      movies = Movie.select "DISTINCT rating"
+      ratings = []
+      movies.each do |movie|
+         ratings << movie.rating
+      end
+      return ratings
+   end #ratings
+
+   def Movie.rated_and_ordered (rat_selecc, ord)
+      w = ""
+      if rat_selecc==nil then rat_selecc = {} end
+      rat_selecc.keys.each do |rat|
+         w += ("rating = '"+rat.to_s+"'")
+         if (rat != rat_selecc.keys.last) then w += " or " end
+      end #each
+      Movie.where(w).order(ord)
+   end #rated_and_ordered
 end
